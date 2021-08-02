b3227701e56059e5709ccf97fe4561f4b25bc761
SLF <a>
2021-08-03 00:03:29 +0800

    readPlanets implemented

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -5,6 +5,18 @@ public class NBody {
         double radius = in.readDouble();
         return radius;
     }
+    public static Planet[] readPlanets(String file){
+        In in = new In(file);
+
+        int n = in.readInt();
+        double radius = in.readDouble();
+        Planet[] array = new Planet[n];
+        for (int i = 0; i < n; i++){
+            array[i] = new Planet(in.readDouble(), in.readDouble(), in.readDouble(),
+                    in.readDouble(), in.readDouble(), in.readString());
+        }
+        return array;
+    }
     /*public static void main(String[] args){
 
     }*/
