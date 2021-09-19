cc87f1d998b131335367915dbc0ee385091675a2
S <974981742@qq.com>
2021-09-19 16:35:26 +0800

    update

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -10,20 +13,54 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int source;
+    private int target;
+    private Maze maze;
+    private boolean targetFound = false;
+
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        source = m.xyTo1D(sourceX, sourceY);
+        target = m.xyTo1D(targetX, targetY);
+        maze = m;
+        edgeTo[source] = source;
+        distTo[source] = 0;
+
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        marked[source] = true;
+        Queue<Integer> queue = new ArrayDeque<>(maze.V());
+        queue.add(source);
+        while (!queue.isEmpty()) {
+
+            int v = queue.remove();
+            announce();
+
+            if (v == target) {
+                targetFound = true;
+                break;
+            }
+            for (int i : maze.adj(v)) {
+                if (!marked[i]) {
+                    marked[i] = true;   //入栈的时候将makred设置为true
+                    queue.add(i);
+                    distTo[i] = distTo[v] + 1;
+                    edgeTo[i] = v;
+                }
+
+            }
+        }
+
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+         bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,10 @@
 package lab11.graphs;
 
+import java.util.ArrayList;
+import java.util.List;
+/*
+ * 可以判断有环图，但是画循环部分的线还有一些bug
+ */
 /**
  *  @author Josh Hug
  */
@@ -9,16 +14,52 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
+    private Maze maze;
+    private boolean cycleFound = false;
+    private int firstCycledNode;
+    private List<Integer> arr;
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+
+        edgeTo[0] = 0;
+        arr = new ArrayList<>(maze.V());
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        arr.add(0);
+        DFS(0,0);
+        while (arr.get(0) != firstCycledNode) {
+            arr.remove(0);
+        }
+        for (int i = 0; i < arr.size() - 1; i++){
+            edgeTo[arr.get(i+1)] = arr.get(i);
+        }
+        edgeTo[arr.get(0)] = arr.get(arr.size() - 1);
+        announce();
     }
 
     // Helper methods go here
+    private void DFS(int s, int p) {
+        marked[s] = true;
+        announce();
+        for (int v : maze.adj(s)) {
+            if (marked[v] && v != p) {
+                cycleFound = true;
+                firstCycledNode = v;
+            }
+            if (!marked[v]) {
+                arr.add(v);
+                DFS(v, s);
+                if (cycleFound) {
+                    return;
+                }
+            }
+
+        }
+
+    }
 }
 
