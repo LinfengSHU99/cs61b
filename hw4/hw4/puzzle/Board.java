20589f2220b73ceca42f2442c7b8afa0af79304c
S <974981742@qq.com>
2021-09-20 17:04:17 +0800

    update

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,10 +1,130 @@
 package hw4.puzzle;
-
-public class Board {
+import edu.princeton.cs.algs4.Queue;
+public class Board implements WorldState{
 
     /** Returns the string representation of the board. 
       * Uncomment this method. */
-    /*public String toString() {
+    private int[][] arr;
+    private final int  BLANK = 0;
+    //private int[][] goal;
+    public Board(int[][] tiles) {
+        arr = new int[tiles.length][tiles.length];
+        for (int i = 0; i < size(); i++) {
+            if (size() >= 0) System.arraycopy(tiles[i], 0, arr[i], 0, size());
+        }
+
+    }
+
+    public int tileAt(int i, int j) {
+        if (i <= size() - 1 && i >= 0 && j <= size() - 1 && j >= 0) {
+            return arr[i][j];
+        }
+        else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    public int size() {
+        return arr.length;
+    }
+
+    public int hamming() {
+        int cnt = 0;
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < size(); j++) {
+                if (i == j && i == size() - 1) {
+                    if (tileAt(i, j) != 0)  cnt++;
+                }
+                else{
+                    if (tileAt(i, j) != i * size() + j + 1){
+                        cnt++;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+
+    public int manhattan() {
+        int cnt = 0;
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < size(); j++) {
+                //int correctValue = i * size() + j + 1;
+                int value = tileAt(i, j);
+                int indexI = (value - 1) / size();
+                int indexJ = value - indexI * size() - 1;
+                if (value == 0) {
+                    indexI = size() - 1;
+                    indexJ = size() - 1;
+                }
+                cnt += (Math.abs(i - indexI) + Math.abs( j - indexJ));
+            }
+        }
+        return cnt;
+    }
+
+    public int estimatedDistanceToGoal() {
+        return manhattan();
+    }
+
+    public boolean equals(Object y) {
+        if (this.getClass() != y.getClass()) {
+            return false;
+        }
+        Board b = (Board) y;
+        if (size() != b.size()) {
+            return false;
+        }
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < size(); j++) {
+                if (tileAt(i, j) != b.tileAt(i, j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Returns neighbors of this board.
+     * SPOILERZ: This is the answer.
+     */
+    @Override
+    public Iterable<WorldState> neighbors() {
+        Queue<WorldState> neighbors = new Queue<>();
+        int hug = size();
+        int bug = -1;
+        int zug = -1;
+        for (int rug = 0; rug < hug; rug++) {
+            for (int tug = 0; tug < hug; tug++) {
+                if (tileAt(rug, tug) == BLANK) {
+                    bug = rug;
+                    zug = tug;
+                }
+            }
+        }
+        int[][] ili1li1 = new int[hug][hug];
+        for (int pug = 0; pug < hug; pug++) {
+            for (int yug = 0; yug < hug; yug++) {
+                ili1li1[pug][yug] = tileAt(pug, yug);
+            }
+        }
+        for (int l11il = 0; l11il < hug; l11il++) {
+            for (int lil1il1 = 0; lil1il1 < hug; lil1il1++) {
+                if (Math.abs(-bug + l11il) + Math.abs(lil1il1 - zug) - 1 == 0) {
+                    ili1li1[bug][zug] = ili1li1[l11il][lil1il1];
+                    ili1li1[l11il][lil1il1] = BLANK;
+                    Board neighbor = new Board(ili1li1);
+                    neighbors.enqueue(neighbor);
+                    ili1li1[l11il][lil1il1] = ili1li1[bug][zug];
+                    ili1li1[bug][zug] = BLANK;
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
@@ -16,6 +136,6 @@ public class Board {
         }
         s.append("\n");
         return s.toString();
-    }*/
+    }
 
 }
---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,88 @@
+package hw4.puzzle;
+
+
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Solver {
+    private class SearchNode implements Comparable<SearchNode> {
+        private int move = 0;
+        private SearchNode prior = null;
+        private WorldState word;
+        private int dist;
+        private int layer;
+
+        private SearchNode(WorldState w, SearchNode p) {
+            this.word = w;
+            if (p != null) {
+                layer = p.layer + 1;
+            }
+            prior = p;
+
+            this.move = layer;
+            dist = word.estimatedDistanceToGoal() + move;
+
+        }
+
+        @Override
+        public int compareTo(SearchNode n1) {
+            int x = this.dist - n1.dist;
+            return Integer.compare(x, 0);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this.getClass() != o.getClass()) {
+                return false;
+            }
+            SearchNode n = (SearchNode) o;
+            return n.word.equals(this.word);
+        }
+
+        @Override
+        public int hashCode() {
+            return word.hashCode();
+        }
+    }
+
+    private LinkedList<WorldState> lst = new LinkedList<>();
+    private MinPQ<SearchNode> pq = new MinPQ<>();
+    private SearchNode finalNode;
+    private HashSet<SearchNode> set = new HashSet<>();
+
+    public Solver(WorldState initial) {
+        SearchNode t = new SearchNode(initial, null);
+        set.add(t);
+        pq.insert(t);
+        finalNode = t;
+
+        while (!initial.isGoal()) {
+            for (WorldState w : initial.neighbors()) {
+                t = new SearchNode(w, finalNode);
+                if (!set.contains(t)) {
+                    pq.insert(t);
+                    //System.out.println(t.word);
+                    set.add(t);
+                }
+            }
+            finalNode = pq.delMin();
+            initial = finalNode.word;
+        }
+
+        SearchNode p = finalNode;
+        while (p != null) {
+            lst.addFirst(p.word);
+            p = p.prior;
+        }
+    }
+
+    public int moves() {
+        return finalNode.layer + 1;
+    }
+
+    public Iterable<WorldState> solution() {
+        return lst;
+    }
+}
